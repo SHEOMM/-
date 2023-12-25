3205dfd6b0f167dfcace04e0fe3a5887ca8d39ed
SHEOMM <esh0821@snu.ac.kr>
2023-12-25 23:54:51 +0900

    test: input 결정자 테스트 코드 구현

--- StringCalculator/src/test/java/org/example/InputDeterminerTest.java
+++ StringCalculator/src/test/java/org/example/InputDeterminerTest.java
@@ -2,47 +2,47 @@ package org.example;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class InputDeterminerTest {
+
+    InputDeterminer inputDeterminer;
+
+    @BeforeEach
+    void setInputDeterminer(){
+        List<InputStrategy> inputDeterminers = new LinkedList<>();
+        inputDeterminers.add(new CommaDelimiter());
+        inputDeterminers.add(new ColonDelimiter());
+        inputDeterminers.add(new CustomDelimiter());
+        this.inputDeterminer = new InputDeterminer(inputDeterminers);
+    }
 
     @Test
     void 콜론패턴매치테스트(){
 
         String input = "1:2:3";
-        Matcher matcher = Pattern.compile("[0-9]*:[0-9]*").matcher(input);
-        if(matcher.find()){
-            Assertions.assertEquals(matcher.regionEnd(), input.length());
-        }else{
-            throw new RuntimeException();
-        }
+        Assertions.assertEquals(":", this.inputDeterminer.getInputType(input));
+
     }
 
     @Test
     void 콤마패턴매치테스트(){
         String input = "1,2,3";
-        Matcher matcher = Pattern.compile("[0-9]*,[0-9]*").matcher(input);
-        if(matcher.find()){
-            Assertions.assertEquals(matcher.regionEnd(), input.length());
-        }else{
-            throw new RuntimeException();
-        }
+        Assertions.assertEquals(",", this.inputDeterminer.getInputType(input));
     }
 
 
     @Test
     void 커스텀구분자패턴매치테스트(){
         String input = "//;\n1;2;3;";
-        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(input);
-        if(matcher.find()){
-            String delimter = matcher.group(1);
-            Assertions.assertEquals(delimter, ";");
-        }else{
-            throw new RuntimeException();
-        }
+        Assertions.assertEquals(";", this.inputDeterminer.getInputType(input));
 
     }
 
