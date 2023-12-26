219696f25a3716500733c11f89869fb29e5cee06
SHEOMM <esh0821@snu.ac.kr>
2023-12-26 16:10:42 +0900

    feat: http header parser

--- web-application-server-master/src/main/java/webserver/RequestHandler.java
+++ web-application-server-master/src/main/java/webserver/RequestHandler.java
@@ -1,11 +1,18 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,6 +30,28 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+            InputStreamReader inputStreamReader = new InputStreamReader(in);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            Map<String, List<String>> headers = new HashMap<>();
+
+            while(true){
+                String rawHeader = bufferedReader.readLine();
+                if(rawHeader.isEmpty())
+                    break;
+
+                List<String> values = new LinkedList<>();
+                String[] parsedHeaders = rawHeader.split(" ");
+                String key = parsedHeaders[0];
+                if (key.endsWith(":")){
+                    key = key.substring(0, key.length() - 1);
+                }
+                values.addAll(Arrays.asList(parsedHeaders).subList(1, parsedHeaders.length));
+                System.out.println(key);
+                System.out.println(values);
+
+                headers.put(key, values);
+            }
+
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "Hello World".getBytes();
